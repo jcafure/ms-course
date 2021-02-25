1afd622a0d7b4074590b91a9e8396d5bcdd34867
jaime cafure <cafure.jaime@gmail.com>
2021-02-25 13:26:19 -0400

    Adicionando feign client no projeto e retirando o rest template

--- hr-payrow/src/main/java/com/devsuperior/hrpayrow/service/PaymentService.java
+++ hr-payrow/src/main/java/com/devsuperior/hrpayrow/service/PaymentService.java
@@ -2,27 +2,20 @@ package com.devsuperior.hrpayrow.service;
 
 import com.devsuperior.hrpayrow.entity.Payment;
 import com.devsuperior.hrpayrow.entity.Worker;
+import com.devsuperior.hrpayrow.feign.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
 
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return Payment.builder().name(worker.getName()).dailyIncome(worker.getDailyIncome()).days(days).build();
     }
 }
